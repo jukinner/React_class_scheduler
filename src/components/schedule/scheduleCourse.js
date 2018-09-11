2fc6e7e2793c0c1999f8f7056721552dea98adac
Jukinner <jukinner@gmail.com>
2018-09-10 21:36:10 -0400

    built out beginning of schedule component

---
+++ src/components/schedule/scheduleCourse.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ScheduleCourse extends Component {
+  render () {
+    return (
+      <div className='schedule-course'>
+        <label>{this.props.title}</label>
+      </div>
+    )
+  }
+}
+
+export default ScheduleCourse;
