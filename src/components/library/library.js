02d5ddf72ea352537fe51aded8a450b5bb8217a7
Justin Kinner <jkinner@gmail.com>
2018-09-15 11:03:04 -0600

    added fonts, font and color variables, styled library course-title, and gave course library an inset

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -22,7 +22,7 @@ class Library extends Component {
   render() {
     return (
         <div className="library">
-          <h1 className="library_title">Course Library</h1>
+          <h1 className="library__title">Course Library</h1>
           	{ this.renderCourses() }
         </div>
       )
