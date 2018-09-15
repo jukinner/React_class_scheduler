fa187ed1a08e4dff3db5250be1c6fd206b0a32e0
Justin Kinner <jkinner@gmail.com>
2018-09-15 11:29:14 -0600

    styled library course component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -14,6 +14,7 @@ class LibraryCourse extends Component {
           <label className="library-course__title">{ this.props.title }</label>
           {Icon("fas fa-check", "library-course__icon")}
         </div>
+        <div className="library-course__line"></div>
         <Arrow className="library-course__arrow" />
         <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="library-course__action"/>
         <div className="library-course__description">
