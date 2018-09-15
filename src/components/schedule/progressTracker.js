0c2a6d712a094934b5eefd49c268ff09248edff2
Justin Kinner <jkinner@gmail.com>
2018-09-15 10:23:58 -0600

    finished functionality with the exception of a couple soon to be found bugs

--- src/components/schedule/progressTracker.js
+++ src/components/schedule/progressTracker.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class ProgressTracker extends Component {
+
+  calculateProgress = function () {
+    return (this.props.amountEnrolled / this.props.amountPossible) * 100;
+  }.bind(this);
+
   render() {
     return (
       <div className="pogress-tracker">
         <label className="progress-tracker__label">Progress Tracker</label>
         <div className="progress-tracker_percentage">
-          0%
+          {this.calculateProgress()}%
         </div>
       </div>
     )
--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -3,36 +3,38 @@ import { connect } from 'react-redux';
 
 import Gradient from '../gradient';
 import ScheduleCourse from './scheduleCourse';
-import ProgressTracker from './progressTracker.js';
+import ProgressTracker from './progressTracker';
 
 class Schedule extends Component {
 
-  renderCourses() {
-    const data = this.props.courses
+    renderCourses() {
+        const data = this.props.courses
 
-    return data.map((course, index) => {
-      if(course.enrolled) {
-        return <ScheduleCourse {...course} key={index}/>
-      }
-    })
-  }
+        return data.map((course, index) => {
+          if(course.enrolled && this.amountErnolled != 5) {
+            this.amountEnrolled++;  
+            return <ScheduleCourse {...course} key={index}/>
+            }
+        })
+    }
 
-  render() {
-    return (
-        <div className="schedule">
-          <h1 className="schedule_title">My Schedule</h1>
-          { this.renderCourses() }
-          <ProgressTracker/>
-          <Gradient/>
-        </div>
-      )
-  }
-}
+    render() {
+        this.amountEnrolled = 0
+        return (
+            <div className="schedule">
+                <h1 className="schedule__title">My Schedule</h1>
+                { this.renderCourses() }
+                <ProgressTracker amountPossible={5} amountEnrolled={this.amountEnrolled}/>
+                <Gradient/>
+            </div>
+        )
+    }
+ }
 
 function mapStateToProps(state) {
-  return {
-    courses: state.courses
-  }
+    return {
+        courses: state.courses
+    }
 }
 
 export default connect(mapStateToProps)(Schedule);
