bcc47efda5947b3b7565aecc2cf6520aef9dd657
Justin Kinner <jkinner@gmail.com>
2018-09-17 17:50:18 -0600

    added course selected styles

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -26,16 +26,21 @@ class LibraryCourse extends Component {
           </p>
         </div>
       )
-    }
+    } 
   }.bind(this);
 
   handleCallback = function(status) {
+    if(!status) {
+      document.getElementById('library-course').classList.add('library-course-selected');
+    } else {
+      document.getElementById('library-course').classList.remove('library-course-selected');
+    }
     this.setState({status})
   }.bind(this);
 
   render() {
     return (
-      <div className="library-course">
+      <div id='library-course' className="library-course">
         <div className="library-course__title-check">
           <label className="library-course__title">{ this.props.title }</label>
           {Icon("fas fa-check", "library-course__icon")}
