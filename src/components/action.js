a66760352faf5491665f17c7912cd95e2d5e5068
Justin Kinner <jkinner@gmail.com>
2018-09-18 16:14:35 -0600

    added action to course library add button

--- src/components/action.js
+++ src/components/action.js
@@ -3,28 +3,39 @@ import Icon from './icon';
 
 class Action extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.status = false;
+    }
+
     handleAction = function() {
         this.props.onClick()
-
-        // document.getElementById('action').classList.add('')
+        if(!this.status) {
+            document.getElementById(this.id).classList.add('action-remove');
+        } else {
+            document.getElementById(this.id).classList.remove('action-remove');
+        }
+        this.status = !this.status;
     }.bind(this);
 
     render() {
+        this.id = `action ${this.props.id}`
         return (
-            <a 
-                id="action"
-                onClick={() => this.props.onClick()} 
+            <a
+                id={this.id}
+                onClick={() => this.handleAction()}
                 className={`${this.props.className} action`}>
-
+            
             </a>
         )
+        // return (
+        //     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+        //         { Icon('fas fa-plus-circle') }
+        //         {/* fas fa-times-circle */}
+        //     </button>
+        // )
     }
 }
-// return (
-//     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-//         { Icon('fas fa-plus-circle') }
-//         {/* fas fa-times-circle */}
-//     </button>
-// )
 
 export default Action;
