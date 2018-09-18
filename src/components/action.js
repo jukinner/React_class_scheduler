83f64f459eec4a1c0ee52443c0c4f1e10d936b54
Justin Kinner <jkinner@gmail.com>
2018-09-17 18:02:59 -0600

    stypled add and remove button

--- src/components/action.js
+++ src/components/action.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
+
+    handleAction = function() {
+        this.props.onClick()
+
+        // document.getElementById('action').classList.add('')
+    }.bind(this);
+
     render() {
         return (
-            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                { Icon('fas fa-plus-circle') }
-                {/* fas fa-times-circle */}
-            </button>
+            <a 
+                id="action"
+                onClick={() => this.props.onClick()} 
+                className={`${this.props.className} action`}>
+
+            </a>
         )
     }
 }
+// return (
+//     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+//         { Icon('fas fa-plus-circle') }
+//         {/* fas fa-times-circle */}
+//     </button>
+// )
 
 export default Action;
