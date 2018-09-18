12d54ac8c8b356d5289639814929f66124a8a095
Justin Kinner <jkinner@gmail.com>
2018-09-18 16:33:51 -0600

    add vviews react animate height library to animate paragraph height

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -6,41 +6,37 @@ import Icon from "../icon";
 import Arrow from "../arrow";
 import Action from "../action";
 
+import AnimateHeight from 'react-animate-height';
 class LibraryCourse extends Component {
 
   constructor(props) {
     super(props)
 
     this.state = {
-      status: true
-    }
+      status: true,
+      height: 0
+    };
   }
 
-  renderDescription = function () {
-    if(!this.state.status) {
-      return (
-        <div className="library-course__description">
-          <label>Course Description</label>
-          <p>
-            { this.props.description }
-          </p>
-        </div>
-      )
-    } 
-  }.bind(this);
+
 
   handleCallback = function(status) {
+    let height = this.state.height == 0 ? 80 : 0 
     if(!status) {
-      document.getElementById('library-course').classList.add('library-course-selected');
+      document.getElementById(this.id).classList.add('library-course-selected');
     } else {
-      document.getElementById('library-course').classList.remove('library-course-selected');
+      document.getElementById(this.id).classList.remove('library-course-selected');
     }
-    this.setState({status})
+    this.setState({
+      status,
+      height
+      });
   }.bind(this);
 
   render() {
+    this.id = `library-course-${this.props.id}`
     return (
-      <div id='library-course' className="library-course">
+      <div id={this.id} className="library-course">
         <div className="library-course__title-check">
           <label className="library-course__title">{ this.props.title }</label>
           {Icon("fas fa-check", "library-course__icon")}
@@ -48,7 +44,15 @@ class LibraryCourse extends Component {
         <div className="library-course__line"></div>
         <Arrow callback={status => this.handleCallback(status)} id={this.props.id} className="library-course__arrow" />
         <Action id={this.props.id} onClick={() => this.props.toggleEnrolled(this.props.id)} className="library-course__action"/>
-        { this.renderDescription()}
+        <AnimateHeight
+          duration={300}
+          height={this.state.height}
+        >
+          <label>Course Description</label>
+          <p>
+            { this.props.description }
+          </p>
+        </AnimateHeight>
       </div>
     );
   }
